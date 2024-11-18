9c3a37e3d636e244081b05d1325f1c9fa2a332f2
christagabriella <s22210400@student.unklab.ac.id>
2024-11-18 15:59:28 +0800

    christa

---
+++ src/component/About/index.js
@@ -0,0 +1,14 @@
+const About = () => {
+    return (
+        <div id="about">
+        <div>
+            <p>
+            The car showroom is a sleek, modern space designed to showcase a variety of vehicles in a polished and professional environment. The large, glass-fronted facade allows natural light to flood the interior, highlighting the gleaming finishes of the cars on display. Inside, the showroom floor is spacious and meticulously clean, with a glossy tile or polished concrete surface that reflects the vehicles.
+            </p>
+        </div>
+        </div>
+    )
+};
+
+export default About;
+
