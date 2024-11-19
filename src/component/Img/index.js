c93b15ca670c6c6c0d5caa49e49ee4b24bb9df5b
AndreasResa <jrresa11@gmail.com>
2024-11-20 00:29:17 +0800

    Tim Push

--- src/component/Img/index.js
+++ src/component/Img/index.js
@@ -1,8 +1,24 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
 const Img = () => {
+    const [image, setImage] = useState({});
+    // const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      const db = getDatabase();
+      // setLoading(true);
+      const imageRef = ref(db, 'image');
+      onValue(imageRef, (snapshot) => {
+        const data = snapshot.val();
+        setImage(data);
+        // setLoading(false);
+      });
+    }, []);
     return (
         <img
         className="coverimage"
-        src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/model/aventador/aventador-sv/slider/aventador-sv.jpg"
+        src={`data:image/jpg;base64, ${image.link}`}
         alt="Cover Image"
         width="100%"
         height="336px"
--- src/component/Name/index.js
+++ src/component/Name/index.js
@@ -1,8 +1,24 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
 const Name = () => {
+    const [name, setName] = useState({});
+    // const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      const db = getDatabase();
+      // setLoading(true);
+      const nameRef = ref(db, "name");
+      onValue(nameRef, (snapshot) => {
+        const data = snapshot.val();
+        setName(data);
+        // setLoading(false);
+      });
+    }, []);
     return (
         <div id="Name">
             <div>
-            <h1> Showroom SSCR </h1>
+            <h1>{name.h1}</h1>
             </div>
         </div>
     )
